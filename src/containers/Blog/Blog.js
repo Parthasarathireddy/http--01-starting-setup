5aeea4dd8a91e84e18d59d323b24bd658a5c9746
parthasarathi <me.parthu@gmail.com>
2020-07-25 13:55:50 +0530

    loading Routes lazyily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 //here replacing with Axios instance;
 //import axios from 'axios';
 import axios from '../../Axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+//in React 16.6 lazyloading like below
+//const NewPost  =React.lazy(() => import ('./NewPost/NewPost'))
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 class Blog extends Component {
     state = {
-        auth : false
+        auth : true
     }
     render () {
         return (
@@ -31,8 +37,12 @@ class Blog extends Component {
                 {/* <Route path = '/' exact render = {() => <Posts />}/> */}
                 {/* Switch will help you to load one component at a time and while using Swith order also importent */}
                 <Switch>
-                    {this.state.auth ? <Route path = '/new-post' component = {NewPost}/> : null}
+                    {this.state.auth ? <Route path = '/new-post' component = {AsyncNewPost}/> : null}
                     <Route path = '/posts' component = {Posts} />
+                    {/* in React 16.6 lazyloading like below */}
+                    {/* <Route render = {() => {
+                        <Suspense fallback = {<div>Loading..</div>}><NewPost/></Suspense>
+                    }}/> */}
                     {/* <Redirect from = '/' to = '/posts' /> */}
                     <Route render = {() => <h1>Not Found!</h1>}/>
                 </Switch>
